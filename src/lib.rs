f42453b951155af04a31673e85622e445575f7c7
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2024-02-17 15:03:39 +0100

    chore: remove todo from sourcecode

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,2 @@
-//! # todo
-//!
-//! - Structs
-//!   - Generics
-//! - Traits
-//!   - Generics/Associated types
-//! - Impls
-//!   - Generics/Associated types
-//!   - Associated constants
-//! - Functions
-//!   - Blocks/Statements
-//!   - Generics
-//! - Enums
-//! - Code generation
-
 pub mod generator;
 pub mod nodes;
